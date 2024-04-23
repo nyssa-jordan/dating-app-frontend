84ecfee19673ab43dc23df0d1858fc7953351f58
nyssa-jordan <jordannyssa@gmail.com>
2024-04-22 20:08:28 -0600

    store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Header.css'
 import PersonIcon from '@mui/icons-material/Person'
 import IconButton from '@mui/material/IconButton'
 import ForumIcon from '@mui/icons-material/Forum'
+import axios from './axios'
 const Header = () => {
+    const [favorites, setFavorites] = useState('');
+    const [blocks, setBlocks] = useState('');
+
+    useEffect(() => {
+        async function fetchFavorites() {
+          const req = await axios.get("/dating/favorites")
+          setFavorites(req.data.favorite)
+        }
+        async function fetchBlocks() {
+            const req = await axios.get("/dating/blocks")
+            setBlocks(req.data.block)
+          }
+        fetchFavorites()
+        fetchBlocks()
+      }, [])
+
+    const handleClick = () => {
+        console.log(favorites);
+        console.log(blocks);
+
+        alert("Favorites are: \n"+favorites[0].name+"\n"+favorites[1].name+"\n"+favorites[2].name+"\n"+favorites[3].name);
+        alert("Blocks are: \n"+blocks[0].name+"\n"+blocks[1].name+"\n"+blocks[2].name+"\n"+blocks[3].name);
+    }
+
     return (
         <div className="header">
-            <IconButton size="large">
+            <IconButton size="large" onClick={handleClick}>
                 <PersonIcon fontSize="large" className="header__icon" />
             </IconButton>
             <img className="header__logo" src="logo192.png" alt="header" />
